c9b15352730141bd58c78f17b90c52d28a765688
Zach Gutierrez <zg71603@gmail.com>
2024-01-10 04:18:57 -0800

    finalizing js

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,6 @@
 //First i need to display time and date in the jumbotron on page load
 //Using moment and the id of currentDay
-$(document).ready(function () 
+$(document).ready(function () {
     
   var now = new Date().getHours();
   var today = moment();
@@ -17,8 +17,6 @@ $(document).ready(function ()
   //Give button onclick that runs a function that saves the input text and
   //adds to local storage.
   //local storage fixed. Reached out to bcs learning assistant and got great help
-  //Issue was on line 28 regarding me targeting only one text area for the value
-  //I am storing
   $(".hourBlock").each(function (i) {
       //console.log(i)
       //console.log(this); 
@@ -41,3 +39,76 @@ $(document).ready(function ()
   $("#hour3 .textSave").val(localStorage.getItem("text-6"))
   $("#hour4 .textSave").val(localStorage.getItem("text-7"))
   $("#hour5 .textSave").val(localStorage.getItem("text-8"))
+  //Then I need to setup the timeblocks/colors for current time, possibly using
+//moment aswell. if else statements possibly that use the current hour vs
+//the time stated in the block and if it is past that time to give it a class
+//or remove a class that colors the field.
+function colorBlocks() {
+  //console.log(now)
+  if (now > 9) {
+    $("#9am").addClass("past");
+  } else if (now >= 9 && now < 10) {
+    $("#9am").addClass("present");
+  } else if (now < 9) {
+    $("#9am").addClass("future");
+  }
+  if (now > 10) {
+    $("#10am").addClass("past");
+  } else if (now >= 10 && now < 11) {
+    $("#10am").addClass("present");
+  } else if (now < 10) {
+    $("#10am").addClass("future");
+  }
+  if (now > 11) {
+    $("#11am").addClass("past");
+  } else if (now >= 11 && now < 12) {
+    $("#11am").addClass("present");
+  } else if (now < 11) {
+    $("#11am").addClass("future");
+  }
+  if (now > 12) {
+    $("#12pm").addClass("past");
+  } else if (now >= 12 && now < 13) {
+    $("#12pm").addClass("present");
+  } else if (now < 12) {
+    $("#12pm").addClass("future");
+  }
+  if (now > 13) {
+    $("#1pm").addClass("past");
+  } else if (now >= 13 && now < 14) {
+    $("#1pm").addClass("present");
+  } else if (now < 13) {
+    $("#1pm").addClass("future");
+  }
+  if (now > 14) {
+    $("#2pm").addClass("past");
+  } else if (now >= 14 && now < 15) {
+    $("#2pm").addClass("present");
+  } else if (now < 14) {
+    $("#2pm").addClass("future");
+  }
+  if (now > 15) {
+    $("#3pm").addClass("past");
+  } else if (now >= 15 && now < 16) {
+    $("#3pm").addClass("present");
+  } else if (now < 15) {
+    $("#3pm").addClass("future");
+  }
+  if (now > 16) {
+    $("#4pm").addClass("past");
+  } else if (now >= 16 && now < 17) {
+    $("#4pm").addClass("present");
+  } else if (now < 16) {
+    $("#4pm").addClass("future");
+  }
+  if (now > 17) {
+    $("#5pm").addClass("past");
+  } else if (now >= 17 && now < 18) {
+    $("#5pm").addClass("present");
+  } else if (now < 17) {
+    $("#5pm").addClass("future");
+  }
+}
+
+colorBlocks();
+})
